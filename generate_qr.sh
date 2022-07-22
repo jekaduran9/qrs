47968b276ec2c848f0380db70c1e6aa6a3b6369e
Jessica Duran <jekaduran9@gmail.com>
2022-07-22 18:44:45 +0200

    fix generate

--- generate_qr.sh
+++ generate_qr.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+set -ex
 IDS=$1
 DIR_OUT=$2
 if [[ -z "$IDS" ]] || [[ -z "$DIR_OUT" ]]; then
@@ -6,7 +7,15 @@ if [[ -z "$IDS" ]] || [[ -z "$DIR_OUT" ]]; then
   echo "for example: generate_qr.sh carmina_ids.tsv carmina"
   exit 1
 fi
-
+rm  $DIR_OUT/*.png || true
 mkdir -p $DIR_OUT
 
-cat $IDS | xargs -I{} sh -c "export URL=\$(echo {} | awk '{print \"https://webapp.yumminn.com/\" \$1 }') && export TABLE=\$(echo {} | awk '{print \$2}') && qrencode -o $DIR_OUT/\$TABLE.black.png \$URL"
+while IFS="" read -r p || [ -n "$p" ]
+do
+  URL=$(echo $p | awk '{print "https://webapp.yumminn.com/" $1}')
+  TABLE=$(echo $p | awk '{print $2}')
+  echo "url: $URL"
+  echo "table: $TABLE"
+  qrencode -o $DIR_OUT/$TABLE.black.png $URL
+done < $IDS
+
