ef63660b8f83faafe4390e2262ebfaaf81a37aa9
Jessica Duran <jekaduran9@gmail.com>
2022-07-22 18:05:31 +0200

    allow receiving arguments

--- change_color.sh
+++ change_color.sh
@@ -1,3 +1,23 @@
 #/bin/bash
-ls *.black.png | xargs -I{} sh -c "convert {} -fill '#FA4515' -opaque 'rgb(0,0,0)' \$(echo {} | sed 's/.black.png/.new.png/')"
+set -ex
+
+DIR_OUT=$1
+QR_COLOR=$2
+BACKGROUND_COLOR=${3:-"#ffffff"}
+
+if [[ -z "$DIR_OUT" ]] || [[ -z "$QR_COLOR" ]]; then
+  echo "you must provide a directory with QR codes and a QR color"
+  echo "for example: change_color.sh carmina #ff0000"
+  exit 1
+fi
+
+
+find $DIR_OUT -name '*.black.png' | xargs -I{} sh -c "convert {} -fill '${QR_COLOR}' -opaque '#000' \$(echo {} | sed 's/.black.png/.new.png/')"
+
+OPTS="-fill '${BACKGROUND_COLOR}' -opaque '#fff'"
+if [[ "$BACKGROUND_COLOR" = "transparent" ]]; then
+  OPTS="-transparent '#fff'"
+fi
+
+find $DIR_OUT -name '*.new.png' | xargs -I{} sh -c "export TMP=\$(mktemp) && convert {} $OPTS \$TMP && mv \$TMP {}"
 
--- generate_qr.sh
+++ generate_qr.sh
@@ -1,3 +1,12 @@
 #/bin/bash
+IDS=$1
+DIR_OUT=$2
+if [[ -z "$IDS" ]] || [[ -z "$DIR_OUT" ]]; then
+  echo "you must provide a tsv file with the IDs and an output directory"
+  echo "for example: generate_qr.sh carmina_ids.tsv carmina"
+  exit 1
+fi
 
-cat ids.tsv | xargs -I{} sh -c "export URL=\$(echo {} | awk '{print \"https://webapp.yumminn.com/\" \$1 }') && export TABLE=\$(echo {} | awk '{print \$2}') && qrencode -o \$TABLE.black.png \$URL"
+mkdir -p $DIR_OUT
+
+cat $IDS | xargs -I{} sh -c "export URL=\$(echo {} | awk '{print \"https://webapp.yumminn.com/\" \$1 }') && export TABLE=\$(echo {} | awk '{print \$2}') && qrencode -o $DIR_OUT/\$TABLE.black.png \$URL"
